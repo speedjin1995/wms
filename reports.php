f2f34a2e1f46fbcbe219a554584f5bcaa1f815af
speedjin1995 <speedjin1995@gmail.com>
2024-07-13 19:49:38 +0800

    added reports

---
+++ reports.php
@@ -0,0 +1,141 @@
+<?php
+require_once 'php/db_connect.php';
+
+session_start();
+
+if(!isset($_SESSION['userID'])){
+  echo '<script type="text/javascript">';
+  echo 'window.location.href = "login.html";</script>';
+}
+else{
+  $user = $_SESSION['userID'];
+  $company = $_SESSION['customer'];
+  $stmt = $db->prepare("SELECT * from users where id = ?");
+	$stmt->bind_param('s', $user);
+	$stmt->execute();
+	$result = $stmt->get_result();
+  $role = 'NORMAL';
+	
+	if(($row = $result->fetch_assoc()) !== null){
+    $role = $row['role_code'];
+  }
+
+  $products = $db->query("SELECT * FROM products WHERE deleted = '0' AND customer = '$company'");
+  $supplies = $db->query("SELECT * FROM supplies WHERE deleted = '0' AND customer = '$company'");
+}
+?>
+
+
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0 text-dark">Reports</h1>
+      </div><!-- /.col -->
+    </div><!-- /.row -->
+  </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<!-- Main content -->
+<div class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card card-danger">
+          <div class="card-header">
+            <div class="row">
+              <div class="col-6"></div>
+              <div class="col-3">
+                <button type="button" class="btn btn-block bg-gradient-warning btn-sm" id="exportPdf">Export PDF</button>
+              </div>
+              <div class="col-3">
+                <button type="button" class="btn btn-block bg-gradient-warning btn-sm" id="exportExcel">Export Excel</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="card-body">
+            <table id="weightTable" class="table table-bordered table-striped display">
+              <thead>
+                <tr>
+                  <th>Serial <br>No.</th>
+                  <th>Product</th>
+                  <th>Gross <br>Weight</th>
+                  <th>Unit <br>Weight</th>
+                  <th>Count</th>
+                  <th></th>
+                </tr>
+              </thead>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>  
+
+<script>
+$(function () {
+  var table = $("#weightTable").DataTable({
+    "responsive": true,
+    "autoWidth": false,
+    'processing': true,
+    'serverSide': true,
+    'serverMethod': 'post',
+    'searching': true,
+    'order': [[ 1, 'asc' ]],
+    'columnDefs': [ { orderable: false, targets: [0] }],
+    'ajax': {
+        'url':'php/loadCount.php'
+    },
+    'columns': [
+      { data: 'serial_no' },
+      { data: 'product_name' },
+      { data: 'gross' },
+      { data: 'unit' },
+      { data: 'count' },
+      { 
+        data: 'id',
+        render: function ( data, type, row ) {
+          return '<div class="row"><div class="col-3"><button type="button" id="edit'+data+'" onclick="edit('+data+')" class="btn btn-success btn-sm"><i class="fas fa-pen"></i></button></div><div class="col-3"><button type="button" id="print'+data+'" onclick="print('+data+')" class="btn btn-warning btn-sm"><i class="fas fa-print"></i></button></div><div class="col-3"><button type="button" id="deactivate'+data+'" onclick="deactivate('+data+')" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button></div></div>';
+        }
+      }
+    ]
+  });
+
+  // Add event listener for opening and closing details
+  $('#weightTable tbody').on('click', 'td.dt-control', function () {
+    var tr = $(this).closest('tr');
+    var row = table.row( tr );
+
+    if ( row.child.isShown() ) {
+      // This row is already open - close it
+      row.child.hide();
+      tr.removeClass('shown');
+    }
+    else {
+      // Open this row
+      <?php 
+        if($role == "ADMIN"){
+          echo 'row.child( format(row.data()) ).show();tr.addClass("shown");';
+        }
+        else{
+          echo 'row.child( formatNormal(row.data()) ).show();tr.addClass("shown");';
+        }
+      ?>
+    }
+  });
+  
+  //Date picker
+  $('#fromDate').datetimepicker({
+      icons: { time: 'far fa-clock' },
+      format: 'DD/MM/YYYY hh:mm:ss A'
+  });
+
+  $('#toDate').datetimepicker({
+      icons: { time: 'far fa-clock' },
+      format: 'DD/MM/YYYY hh:mm:ss A'
+  });
+});
+</script>
