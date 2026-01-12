3062e2c37df9b68f8d94fc7b17deed9da6cc2cf9
Ng Jih Bin <sky.ng@dcap.my>
2026-01-12 22:43:58 +0800

    added parent to admin company management

--- SQL/deploy.sql
+++ SQL/deploy.sql
@@ -1,4 +1,4 @@
--- 09/01/2025 --
+-- 09/01/2026 --
 CREATE TABLE `grades` (
   `id` int(11) NOT NULL,
   `units` varchar(5) NOT NULL,
@@ -25,7 +25,7 @@ ALTER TABLE `product_customers` ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `product_customers` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
--- 11/01/2025 --
+-- 11/01/2026 --
 ALTER TABLE `users` CHANGE `baudrate` `baudrate` VARCHAR(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL;
 
 ALTER TABLE `users` CHANGE `databits` `databits` VARCHAR(5) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL;
@@ -33,3 +33,6 @@ ALTER TABLE `users` CHANGE `databits` `databits` VARCHAR(5) CHARACTER SET utf8mb
 ALTER TABLE `users` CHANGE `parity` `parity` VARCHAR(5) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL;
 
 ALTER TABLE `users` CHANGE `stopbits` `stopbits` VARCHAR(5) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL;
+
+-- 12/01/2026 --
+ALTER TABLE `companies` ADD `parent` INT(11) NULL AFTER `products`;
