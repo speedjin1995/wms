95c0e4e7056b334cfd6b7e26f0ea3abd2b1d67f1
Ng Jih Bin <sky.ng@dcap.my>
2026-01-20 21:02:44 +0800

    push for status do/po no

--- php/print.php
+++ php/print.php
@@ -282,7 +282,7 @@ if(isset($_POST['userID'])){
                                 <div class="header-row"><span class="header-label">Transaction ID</span><span class="header-value">: '.$wholesale['serial_no'].'</span></div>
                                 <div class="header-row"><span class="header-label">Status</span><span class="header-value">: '.($wholesale['status'] == 'DISPATCH' ? 'Dispatch' : 'Incoming').'</span></div>
                                 <div class="header-row"><span class="header-label">From Date</span><span class="header-value">: '.date('d/m/Y', strtotime($wholesale['created_datetime'])).'</span></div>
-                                <div class="header-row"><span class="header-label">Purchase No</span><span class="header-value">: '.$wholesale['po_no'].'</span></div>';
+                                <div class="header-row"><span class="header-label">'.($wholesale['status'] == 'DISPATCH' ? 'Purchase' : 'Delivery').' No</span><span class="header-value">: '.$wholesale['po_no'].'</span></div>';
 
                                 if ($wholesale['status'] == 'RECEIVING') {
                                     $message .= '
--- php/print_mobile.php
+++ php/print_mobile.php
@@ -285,7 +285,7 @@ if(isset($_GET['userID'])){
                                 <div class="header-row"><span class="header-label">Transaction ID</span><span class="header-value">: '.$wholesale['serial_no'].'</span></div>
                                 <div class="header-row"><span class="header-label">Status</span><span class="header-value">: '.($wholesale['status'] == 'DISPATCH' ? 'Dispatch' : 'Incoming').'</span></div>
                                 <div class="header-row"><span class="header-label">From Date</span><span class="header-value">: '.date('d/m/Y', strtotime($wholesale['created_datetime'])).'</span></div>
-                                <div class="header-row"><span class="header-label">Purchase No</span><span class="header-value">: '.$wholesale['po_no'].'</span></div>';
+                                <div class="header-row"><span class="header-label">'.($wholesale['status'] == 'DISPATCH' ? 'Purchase' : 'Delivery').' No</span><span class="header-value">: '.$wholesale['po_no'].'</span></div>';
 
                                 if ($wholesale['status'] == 'RECEIVING') {
                                     $message .= '
