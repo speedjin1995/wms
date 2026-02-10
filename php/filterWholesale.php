1ae4e8d422cfbe8fd54c4572c4fff3d4bd6fedd2
Ng Jih Bin <sky.ng@dcap.my>
2026-02-10 13:20:45 +0800

    fixed counter total count for filter wb and wholesale

--- php/filterWholesale.php
+++ php/filterWholesale.php
@@ -81,21 +81,24 @@ $company = $_SESSION['customer'];
 $user = $_SESSION['userID'];
 
 if ($user != 2){
-  $searchQuery .= " AND company = '".$company."'";
+  $companyFilter = " AND company = '".$company."'";
+  // $searchQuery .= " AND company = '".$company."'";
+}else{
+  $companyFilter = '';
 }
 
 ## Total number of records without filtering
-$sel = mysqli_query($db,"select count(*) as allcount from wholesales where 1=1");
+$sel = mysqli_query($db,"select count(*) as allcount from wholesales where 1=1".$companyFilter);
 $records = mysqli_fetch_assoc($sel);
 $totalRecords = $records['allcount'];
 
 ## Total number of record with filtering
-$sel = mysqli_query($db,"select count(*) as allcount from wholesales where 1=1".$searchQuery);
+$sel = mysqli_query($db,"select count(*) as allcount from wholesales where 1=1".$companyFilter.$searchQuery);
 $records = mysqli_fetch_assoc($sel);
 $totalRecordwithFilter = $records['allcount'];
 
 ## Fetch records
-$empQuery = "select wholesales.* from wholesales where 1=1".$searchQuery." order by ".$columnName." ".$columnSortOrder." limit ".$row.",".$rowperpage;
+$empQuery = "select wholesales.* from wholesales where 1=1".$companyFilter.$searchQuery." order by ".$columnName." ".$columnSortOrder." limit ".$row.",".$rowperpage;
 $empRecords = mysqli_query($db, $empQuery);
 $data = array();
 
