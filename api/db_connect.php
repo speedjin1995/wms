92b981a3fb552ab909560ac0f604141fc45ac1fe
speedjin1995 <speedjin1995@gmail.com>
2026-02-20 18:16:28 +0800

    removed admin and API

--- api/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-date_default_timezone_set('Asia/Kuala_Lumpur');
-$db = mysqli_connect("localhost", "u664110560_wms", "Aa@111222333", "u664110560_wms");
-
-if(mysqli_connect_errno()){
-    echo 'Database connection failed with following errors: ' . mysqli_connect_error();
-    die();
-}
-?>
