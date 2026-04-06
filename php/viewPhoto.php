84694a0bb28757274fc38395e19a5460a58d00a9
Ng Jih Bin <sky.ng@dcap.my>
2026-04-06 22:06:43 +0800

    add checking for method

--- php/viewPhoto.php
+++ php/viewPhoto.php
@@ -11,23 +11,25 @@ else{
     if (isset($_GET['file']) && !empty($_GET['file'])) {
         $fileId = filter_input(INPUT_GET, 'file', FILTER_SANITIZE_STRING);
     
-        $stmt = $db->prepare("SELECT filepath FROM files WHERE id = ? AND deleted = 0");
+        $stmt = $db->prepare("SELECT * FROM files WHERE id = ? AND deleted = 0");
         $stmt->bind_param('s', $fileId);
         $stmt->execute();
         $result = $stmt->get_result();
     
         if ($row = $result->fetch_assoc()) {
-            $filepath = str_replace('../', '', $row['filepath']);
-            $filePath = str_replace('\\', '/', dirname(__DIR__, 3)) . '/' . $filepath;
-    
-            if (file_exists($filePath)) {
-                header('Content-Type: ' . mime_content_type($filePath));
-                header('Content-Disposition: inline; filename="' . basename($filePath) . '"');
-                readfile($filePath);
-                exit;
-            } 
-            else {
-                echo 'File not found!!.';
+            if ($row['method'] == 'local') {
+                $filepath = str_replace('../', '', $row['filepath']);
+                $filePath = str_replace('\\', '/', dirname(__DIR__, 3)) . '/' . $filepath;
+        
+                if (file_exists($filePath)) {
+                    header('Content-Type: ' . mime_content_type($filePath));
+                    header('Content-Disposition: inline; filename="' . basename($filePath) . '"');
+                    readfile($filePath);
+                    exit;
+                } 
+                else {
+                    echo 'File not found!!.';
+                }
             }
         } 
         else {
