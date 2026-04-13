dd0eb28123b2821d4fb00a28fc57d9343966aa6a
RenaYew <renayewhy@yahoo.com>
2026-04-13 10:26:58 +0800

    UI/UX
    Change footer copyright text

--- index.php
+++ index.php
@@ -625,7 +625,7 @@ to get the desired effect
 
   <!-- Main Footer -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2020 <a href="#">SyncWeight</a> · </strong>All Rights Reserved<div class="float-right d-none d-sm-inline-block"><b>Version</b> 1.0.0 </div>
+    <strong>Copyright &copy; 2020 <a href="#">Idea of SyncWeight</a> · </strong>All Rights Reserved<div class="float-right d-none d-sm-inline-block"><b>Version</b> 1.10 </div>
   </footer>
 </div>
 <!-- ./wrapper -->
