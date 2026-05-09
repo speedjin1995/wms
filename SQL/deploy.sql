488a9760b45626954bebb8fc3d44610e21363699
Ng Jih Bin <sky.ng@dcap.my>
2026-05-09 20:15:48 +0800

    added running no setup sql

--- SQL/deploy.sql
+++ SQL/deploy.sql
@@ -652,9 +652,12 @@ ALTER TABLE `packaging` ADD `packaging_type` VARCHAR(30) NOT NULL DEFAULT 'origi
 CREATE TABLE `statuses` (
   `id` int(11) NOT NULL,
   `module` varchar(100) NOT NULL,
-  `status` varchar(100) NOT NULL
+  `status` varchar(100) NOT NULL,
+  `prefix` VARCHAR(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 ALTER TABLE `statuses` ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `statuses` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `running_no_setup` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT, add PRIMARY KEY (`id`);
