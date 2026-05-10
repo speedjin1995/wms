7837b69752c1a1659a3632e90d1c652548fac7de
Ng Jih Bin <sky.ng@dcap.my>
2026-05-10 10:11:21 +0800

    added setup page for company indicator

--- SQL/deploy.sql
+++ SQL/deploy.sql
@@ -664,3 +664,5 @@ ALTER TABLE `running_no_setup` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT,
 
 -- 10/05/2026 --
 ALTER TABLE `sales` ADD `payments` TEXT NULL AFTER `payment_method`;
+
+ALTER TABLE `companies` ADD `indicator` INT(11) NULL AFTER `photo_upload_mode`;
