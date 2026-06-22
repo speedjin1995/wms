8f78b3e63682da36e04bbbcbc0d82e527477801b
Jihb0105 <ngjihbin1234@gmail.com>
2026-06-22 09:25:01 +0800

    change to start_time instead of created_datetime

--- php/getWholesale.php
+++ php/getWholesale.php
@@ -48,6 +48,7 @@ if(isset($_POST['userID'])){
                 $message['remark'] = $row['remark'];
                 $message['remarks2'] = $row['remarks2'];
                 $message['created_datetime'] = $row['created_datetime'];
+                $message['start_time'] = $row['start_time'];
                 $message['end_time'] = $row['end_time'];
                 $message['records_type'] = $row['records_type'];
                 
